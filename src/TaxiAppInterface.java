c5883e155f1cc1f04b8d91b61c9ecce77bd6bc62
punkt4.3.1 <akimovas21022000@gmail.com>
2025-10-02 13:34:22 +0300

    Create TaxiAppInterface.java

---
+++ src/TaxiAppInterface.java
@@ -0,0 +1,7 @@
+// Отдельный файл для интерфейса
+public interface TaxiAppInterface {
+    void showWelcomeMessage();
+    void processUserInput();
+    void displayResults();
+    boolean isValidInput(String input);
+}
